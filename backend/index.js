356793c6082c9be751d296b11b31c3d3283c4bb9
Lucky yadav <luckyyadav8627@gmail.com>
2023-06-28 20:35:12 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,11 @@ const jwt = require("jsonwebtoken");
 // const { OAuth2Client } = require("google-auth-library");
 const mongoose = require("mongoose");
 const taskRouter = require("./routes/crudRoutes");
-const dotenv = require("dotenv");
+// const dotenv = require("dotenv");
 
 app.use(express.json());
 app.use(cors());
-dotenv.config();
+// dotenv.config();
 app.use("/tasks", taskRouter);
 app.get("/", (req, res) => {
   console.log(res);
@@ -19,7 +19,10 @@ app.get("/", (req, res) => {
 });
 
 mongoose
-  .connect(`${process.env.MongoDb_String}`)
+  .connect(
+    // `${process.env.MongoDb_String}`
+    "mongodb+srv://luckyyadav8627:c9DZjW2L2bNC5I9Q@cluster0.jxj9tsp.mongodb.net/?retryWrites=true&w=majority"
+  )
   .then(() => {
     console.log("Database connection successful");
     app.listen(PORT, () => {
